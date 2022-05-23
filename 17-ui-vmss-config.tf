7d6d3a22eca641286a4c4aa6e4bbd3d3dbe43b52
asyraf <tengku.m.asyraf@gmail.com>
2022-05-24 01:00:05 +0800

    Test Sucessful Pipeline

--- 17-ui-vmss-config.tf
+++ 17-ui-vmss-config.tf
@@ -1,67 +1,67 @@
-# Reference   : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/monitor_autoscale_setting
-# Description : Manages a AutoScale Setting which can be applied to Virtual Machine Scale Sets, App Services and other scalable resources.
-resource "azurerm_monitor_autoscale_setting" "ui-vmss-config" {
-  name                = "${local.group_name}-ui-vmss-config"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  target_resource_id  = azurerm_linux_virtual_machine_scale_set.ui-vmss.id
+# # Reference   : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/monitor_autoscale_setting
+# # Description : Manages a AutoScale Setting which can be applied to Virtual Machine Scale Sets, App Services and other scalable resources.
+# resource "azurerm_monitor_autoscale_setting" "ui-vmss-config" {
+#   name                = "${local.group_name}-ui-vmss-config"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   target_resource_id  = azurerm_linux_virtual_machine_scale_set.ui-vmss.id
 
-  notification {
-    email {
-      send_to_subscription_administrator    = true
-      send_to_subscription_co_administrator = true
-      custom_emails                         = ["tengku.m.asyraf@hotmail.com"]
-    }
-  }
+#   notification {
+#     email {
+#       send_to_subscription_administrator    = true
+#       send_to_subscription_co_administrator = true
+#       custom_emails                         = ["tengku.m.asyraf@hotmail.com"]
+#     }
+#   }
 
-  profile {
-    name = "monitoring-cpu"
-    capacity {
-      default = 2
-      minimum = 1
-      maximum = 2
-    }
+#   profile {
+#     name = "monitoring-cpu"
+#     capacity {
+#       default = 2
+#       minimum = 1
+#       maximum = 2
+#     }
 
-    rule {
-      scale_action {
-        direction = "Increase"
-        type      = "ChangeCount"
-        value     = 1
-        cooldown  = "PT5M"
-      }
-      metric_trigger {
-        metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_linux_virtual_machine_scale_set.ui-vmss.id
-        metric_namespace   = "microsoft.compute/virtualmachinescalesets"
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT5M"
-        time_aggregation   = "Average"
-        operator           = "GreaterThan"
-        threshold          = 75
-      }
-    }
+#     rule {
+#       scale_action {
+#         direction = "Increase"
+#         type      = "ChangeCount"
+#         value     = 1
+#         cooldown  = "PT5M"
+#       }
+#       metric_trigger {
+#         metric_name        = "Percentage CPU"
+#         metric_resource_id = azurerm_linux_virtual_machine_scale_set.ui-vmss.id
+#         metric_namespace   = "microsoft.compute/virtualmachinescalesets"
+#         time_grain         = "PT1M"
+#         statistic          = "Average"
+#         time_window        = "PT5M"
+#         time_aggregation   = "Average"
+#         operator           = "GreaterThan"
+#         threshold          = 75
+#       }
+#     }
 
-    rule {
-      scale_action {
-        direction = "Decrease"
-        type      = "ChangeCount"
-        value     = 1
-        cooldown  = "PT1M"
-      }
-      metric_trigger {
-        metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_linux_virtual_machine_scale_set.ui-vmss.id
-        metric_namespace   = "microsoft.compute/virtualmachinescalesets"
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT1M"
-        time_aggregation   = "Average"
-        operator           = "LessThan"
-        threshold          = 25
-      }
-    }
-  }
+#     rule {
+#       scale_action {
+#         direction = "Decrease"
+#         type      = "ChangeCount"
+#         value     = 1
+#         cooldown  = "PT1M"
+#       }
+#       metric_trigger {
+#         metric_name        = "Percentage CPU"
+#         metric_resource_id = azurerm_linux_virtual_machine_scale_set.ui-vmss.id
+#         metric_namespace   = "microsoft.compute/virtualmachinescalesets"
+#         time_grain         = "PT1M"
+#         statistic          = "Average"
+#         time_window        = "PT1M"
+#         time_aggregation   = "Average"
+#         operator           = "LessThan"
+#         threshold          = 25
+#       }
+#     }
+#   }
 
 
-}
+# }
