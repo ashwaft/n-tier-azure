558a98718de9bcd9b5459e6238d893a44b651338
asyraf <tengku.m.asyraf@gmail.com>
2022-05-24 01:27:19 +0800

    Fix pipeline

--- 06-ui-nsg.tf
+++ 06-ui-nsg.tf
@@ -1,27 +1,27 @@
 # Reference   : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_group
 # Description : Manages a network security group that contains a list of network security rules. 
 #             : Network security groups enable inbound or outbound traffic to be enabled or denied.
-resource "azurerm_network_security_group" "uisubnet_nsg" {
-  name                = "${local.group_name}-uisubnet_nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# resource "azurerm_network_security_group" "uisubnet_nsg" {
+#   name                = "${local.group_name}-uisubnet_nsg"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  dynamic "security_rule" {
-    for_each = local.api_inbound_ports
+#   dynamic "security_rule" {
+#     for_each = local.api_inbound_ports
 
-    content {
-      name                       = "Inbound-rule-${security_rule.key}"
-      description                = "Inbound Rule ${security_rule.key}"
-      priority                   = security_rule.key
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = security_rule.value
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
-  }
-}
+#     content {
+#       name                       = "Inbound-rule-${security_rule.key}"
+#       description                = "Inbound Rule ${security_rule.key}"
+#       priority                   = security_rule.key
+#       direction                  = "Inbound"
+#       access                     = "Allow"
+#       protocol                   = "Tcp"
+#       source_port_range          = "*"
+#       destination_port_range     = security_rule.value
+#       source_address_prefix      = "*"
+#       destination_address_prefix = "*"
+#     }
+#   }
+# }
 
 
--- 07-api-subnet.tf
+++ 07-api-subnet.tf
@@ -1,8 +1,8 @@
 # Reference   : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet
 # Description : Manages a subnet. Subnets represent network segments within the IP space defined by the virtual network.
-resource "azurerm_subnet" "apisubnet" {
-  name                 = "${azurerm_virtual_network.vnet.name}-${var.apisubnet_name}"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = var.apisubnet_address
-}
+# resource "azurerm_subnet" "apisubnet" {
+#   name                 = "${azurerm_virtual_network.vnet.name}-${var.apisubnet_name}"
+#   resource_group_name  = azurerm_resource_group.rg.name
+#   virtual_network_name = azurerm_virtual_network.vnet.name
+#   address_prefixes     = var.apisubnet_address
+# }
