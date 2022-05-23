950e76bc8b230a7534bdecefcd018a6e3451eac3
asyraf <tengku.m.asyraf@gmail.com>
2022-05-24 00:39:06 +0800

    Comment out app gateway

--- 12-application-gateway-nsg.tf
+++ 12-application-gateway-nsg.tf
@@ -1,26 +1,26 @@
 # Reference   : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_group
 # Description : Manages a network security group that contains a list of network security rules. 
 #             : Network security groups enable inbound or outbound traffic to be enabled or denied.
-resource "azurerm_network_security_group" "ag_subnet_nsg" {
-  name                = "${local.group_name}-ag_subnet_nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# resource "azurerm_network_security_group" "ag_subnet_nsg" {
+#   name                = "${local.group_name}-ag_subnet_nsg"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  dynamic "security_rule" {
-    for_each = local.ag_inbound_ports
+#   dynamic "security_rule" {
+#     for_each = local.ag_inbound_ports
 
-    content {
-      name                       = "Inbound-rule-${security_rule.key}"
-      description                = "Inbound Rule ${security_rule.key}"
-      priority                   = security_rule.key
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = security_rule.value
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
-  }
-}
+#     content {
+#       name                       = "Inbound-rule-${security_rule.key}"
+#       description                = "Inbound Rule ${security_rule.key}"
+#       priority                   = security_rule.key
+#       direction                  = "Inbound"
+#       access                     = "Allow"
+#       protocol                   = "Tcp"
+#       source_port_range          = "*"
+#       destination_port_range     = security_rule.value
+#       source_address_prefix      = "*"
+#       destination_address_prefix = "*"
+#     }
+#   }
+# }
 
